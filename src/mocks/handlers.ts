d954a6a1d2ea6d035bb8c6e0bf3f08a5a1570f46
Gabriela Yanes <gabriela.yanes@geopagos.com>
2022-12-04 16:38:21 -0300

    fix misspelling

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 import data from './lender.json';
 
 export const handlers = [
-  // Handles a GET /lender/1 request
+  // Handles a GET /lender/:lenderId request
   rest.get('/api/v1/lender/:lenderId', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(data.lender));
   }),
